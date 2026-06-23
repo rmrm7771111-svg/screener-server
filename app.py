73e0c790753a6e7858bc18bd8c87ab57a0cd2c4d
rmrm7771111-svg <rmrm7771111@gmail.com>
2026-06-23 18:28:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+import os
 
 app = Flask(__name__)
+
+API_KEY = os.environ.get("API_KEY")
 
 @app.route("/")
 def home():
     return {"status": "online"}
+
+@app.route("/quote/<symbol>")
+def quote(symbol):
+
+    url = "https://finnhub.io/api/v1/quote"
+
+    r = requests.get(
+        url,
+        params={
+            "symbol": symbol.upper(),
+            "token": API_KEY
+        }
+    )
+
+    return jsonify(r.json())
